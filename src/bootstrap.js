5eb73ee1e01f2e09117ca7c1c056736a1501e58b
Thomas Andersen <tan@enonic.com>
2012-02-06 20:58:45 +0100

    HelloBootAMD r12833

---
+++ src/bootstrap.js
@@ -0,0 +1,131 @@
+/* See license.txt for terms of usage */
+
+// ********************************************************************************************* //
+// XPCOM
+
+var {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+Cu.import("resource://gre/modules/Services.jsm");
+
+// ********************************************************************************************* //
+// Constants
+
+const BOOTSTRAP_REASONS = [
+    "", // the bootstrap reason is 1 based
+    "APP_STARTUP",
+    "APP_SHUTDOWN",
+    "ADDON_ENABLE",
+    "ADDON_DISABLE",
+    "ADDON_INSTALL",
+    "ADDON_UNINSTALL",
+    "ADDON_UPGRADE",
+    "ADDON_DOWNGRADE"
+];
+
+// ********************************************************************************************* //
+// Firefox Bootstrap API
+
+function install(data, reason) {}
+function uninstall(data, reason) {}
+function startup(data, reason) { firebugStartup(); }
+function shutdown(data, reason) { firebugShutdown(); }
+
+// ********************************************************************************************* //
+// Firebug Bootstrap API
+
+/**
+ * Executed by Firebug framework when Firebug is started. Since the order of Firebug
+ * and its bootstrapped extensions is not guaranteed this function is executed twice
+ * (of course the registration happens just once):
+ *
+ * 1) When Firebug is loaded
+ * 2) When this extension is loaded
+ *
+ * If Firebug is not loaded an exception happens
+ */
+function firebugStartup()
+{
+    try
+    {
+        Cu.import("resource://firebug/loader.js");
+        FirebugLoader.registerBootstrapScope(this);
+    }
+    catch (e)
+    {
+        // If an exception happens it's probably because Firebug hasn't been
+        // started yet. Just ignore it.
+    }
+}
+
+/**
+ * Executed by Firefox when this extension shutdowns.
+ */
+function firebugShutdown()
+{
+    try
+    {
+        Cu.import("resource://firebug/loader.js");
+        FirebugLoader.unregisterBootstrapScope(this);
+    }
+    catch (e)
+    {
+        Cu.reportError(e);
+    }
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+/**
+ * Executed by Firebug framework for every browser window. Use this function to append
+ * any new elements into the browser window (browser.xul). Don't forget to remove
+ * these elements in topWindowUnload.
+ * 
+ * @param {Window} win The browser window
+ */
+function topWindowLoad(win)
+{
+    // TODO: overlay global browser window
+}
+
+/**
+ * Executed by Firebug framework when this extension
+ * @param {Object} win
+ */
+function topWindowUnload(win)
+{
+    // TODO: remove global browser window overlays
+}
+
+/**
+ * Entire Firebug UI is running inside an iframe (firebugFrame.xul). This function
+ * is executed by Firebug framework when the frame is loaded. This happens when
+ * the user requires Firebug for the first time (doesn't have to happen during the
+ * Firefox session at all)
+ * 
+ * @param {Window} win The Firebug window
+ */
+function firebugFrameLoad(Firebug)
+{
+    // The registration process will automatically look for 'main' module and load it.
+    // The is the same what happens in a XUL overlay applied on:
+    // chrome://firebug/content/firebugOverlay.xul
+    var config = {id: "hellobootamd@janodvarko.cz"};
+    Firebug.registerExtension("hellobootamd", config);
+}
+
+function firebugFrameUnload(Firebug)
+{
+    if (!Firebug.isInitialized)
+        return;
+
+    Firebug.unregisterExtension("hellobootamd");
+
+    // Make sure another panel is selected if the current one has been removed.
+    // xxxHonza: should be done automatically by Firebug
+    var panel = Firebug.chrome.getSelectedPanel();
+    if (panel.name == "helloBootAMD")
+        Firebug.chrome.selectPanel("html");
+
+    Firebug.chrome.syncMainPanels();
+}
+
+// ********************************************************************************************* //
---
+++ src/chrome/content/main.js
@@ -0,0 +1,59 @@
+/* See license.txt for terms of usage */
+
+define([
+    "firebug/lib/trace",
+    "firebug/trace/traceModule",
+    "firebug/trace/traceListener",
+    "hellobootamd/myPanel",
+    "hellobootamd/myModule",
+],
+function(FBTrace, TraceModule, TraceListener) {
+
+// ********************************************************************************************* //
+// Documentation
+//
+// Firebug coding style: http://getfirebug.com/wiki/index.php/Coding_Style
+// Firebug tracing: http://getfirebug.com/wiki/index.php/FBTrace
+
+// ********************************************************************************************* //
+// The application/extension object
+
+var theApp =
+{
+    initialize: function()
+    {
+        // Register trace customization listener for FBTrace. DBG_HELLOBOOTAMD represents a CSS rule
+        // that is automatially associated with all logs prefixed with "helloBootAMD;".
+        // The prefix is removed (third parameter is true).
+        // The last parameter represents URL of the stylesheet that should be used by
+        // the tracing console.
+        this.traceListener = new TraceListener("helloBootAMD;", "DBG_HELLOBOOTAMD", true,
+            "chrome:/hellobootamd/skin/hellobootamd.css");
+        TraceModule.addListener(this.traceListener);
+
+        if (FBTrace.DBG_HELLOBOOTAMD)
+            FBTrace.sysout("helloBootAMD; HelloBootAMD extension initialize");
+
+        // TODO: Extension initialization
+    },
+
+    shutdown: function()
+    {
+        if (FBTrace.DBG_HELLOBOOTAMD)
+            FBTrace.sysout("helloBootAMD; HelloBootAMD extension shutdown");
+
+        // Unregister all registered Firebug components
+
+        Firebug.unregisterStylesheet("chrome://hellobootamd/skin/hellobootamd.css");
+        Firebug.unregisterStringBundle("chrome://hellobootamd/locale/hellobootamd.properties");
+
+        // TODO: Extension shutdown
+
+        TraceModule.removeListener(this.traceListener);
+    }
+}
+
+return theApp;
+
+// ********************************************************************************************* //
+});
